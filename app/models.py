3a6830fdc6fb1dd3d4ae1aacd2b4e0abd00f6b38
NessaAz <vanessa.azenwa@gmail.com>
2022-05-04 14:28:47 +0300

    add definition to class Article in models module

---
+++ app/models.py
@@ -0,0 +1,12 @@
+class Article:
+    
+    def __init__(self, source, author, title, description, url, url_to_image, published_at):
+        self.source = source
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.url_to_image = url_to_image
+        self.published_at = published_at
+
+
