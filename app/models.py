fdbc125772d92a69d49b1fd8c0eb9689326a999f
NessaAz <vanessa.azenwa@gmail.com>
2022-05-04 14:33:38 +0300

    define Source class

--- app/models.py
+++ app/models.py
@@ -10,3 +10,12 @@ class Article:
         self.published_at = published_at
 
 
+class Source:
+    
+    def __init__(self, id, name, description, url, category, country):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
