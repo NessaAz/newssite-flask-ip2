09334c9a6e4c838fd486593ba6aa9d9000f5be92
NessaAz <vanessa.azenwa@gmail.com>
2022-05-04 13:01:19 +0300

    connect application to the instance config file

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from app.config import config_options
 
 # Initializing application
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config = True)
 
 from app import views
 
